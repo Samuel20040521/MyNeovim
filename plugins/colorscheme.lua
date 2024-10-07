bd60ecd811497143ac90d15e12e9427d3430ccb9
Polarbear <b11202015@g.ntu.edu.tw>
2024-10-08 00:50:24 +0800

    transparent feature
    我將背景設為透明，並且調整了telescope的透明，以及調整comment的顏色，避免太淺看不到。

--- plugins/colorscheme.lua
+++ plugins/colorscheme.lua
@@ -12,26 +12,28 @@ return {
       local fg_dark = "#B4D0E9"
       local fg_gutter = "#627E97"
       local border = "#547998"
+      local comment = "#636974"
 
       require("tokyonight").setup({
         transparent = true,
         style = "night",
         on_colors = function(colors)
-          colors.bg = bg
-          colors.bg_dark = bg_dark
-          colors.bg_float = bg_dark
-          colors.bg_highlight = bg_highlight
-          colors.bg_popup = bg_dark
-          colors.bg_search = bg_search
-          colors.bg_sidebar = bg_dark
-          colors.bg_statusline = bg_dark
-          colors.bg_visual = bg_visual
+          -- colors.bg = bg
+          -- colors.bg_dark = bg_dark
+          -- colors.bg_float = bg_dark
+          -- colors.bg_highlight = bg_highlight
+          -- colors.bg_popup = bg_dark
+          -- colors.bg_search = bg_search
+          -- colors.bg_sidebar = bg_dark
+          -- colors.bg_statusline = bg_dark
+          -- colors.bg_visual = bg_visual
           colors.border = border
           colors.fg = fg
           colors.fg_dark = fg_dark
           colors.fg_float = fg
           colors.fg_gutter = fg_gutter
           colors.fg_sidebar = fg_dark
+          colors.comment = comment
         end,
         styles = {
           sidebars = "transparent",
