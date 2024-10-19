7a26b7cf2e7624a9680bbbb57740b07a238a711c
Polarbear <b11202015@g.ntu.edu.tw>
2024-10-19 18:15:09 +0800

    let autocompletetion length adjustable

--- plugins/nvim-cmp.lua
+++ plugins/nvim-cmp.lua
@@ -3,7 +3,7 @@ return {
   event = "InsertEnter",
   dependencies = {
     "hrsh7th/cmp-buffer", -- source for text in buffer
-    "hrsh7th/cmp-path", -- source for file system paths
+    "hrsh7th/cmp-path",   -- source for file system paths
     {
       "L3MON4D3/LuaSnip",
       -- follow latest release.
@@ -11,11 +11,11 @@ return {
       -- install jsregexp (optional!).
       build = "make install_jsregexp",
     },
-    "saadparwaiz1/cmp_luasnip", -- for autocompletion
+    "saadparwaiz1/cmp_luasnip",     -- for autocompletion
     "rafamadriz/friendly-snippets", -- useful snippets
-    "onsails/lspkind.nvim", -- vs-code like pictograms
+    "onsails/lspkind.nvim",         -- vs-code like pictograms
   },
-  
+
   config = function()
     local cmp = require("cmp")
     local luasnip = require("luasnip")
@@ -29,14 +29,14 @@ return {
         expand = function(args)
           luasnip.lsp_expand(args.body)
           -- Anatomy of a LuaSnip snippet
-         -- Define snip_params
+          -- Define snip_params
           --local snip_params = args.snippet
           --local nodes = args.nodes
           --local opts = args.opts
           --require("luasnip").snippet(
-           -- snip_params(table),  -- table of snippet parameters
-            --nodes(table),        -- table of snippet nodes
-            --opts(table|nil)      -- *optional* table of additional snippet options
+          -- snip_params(table),  -- table of snippet parameters
+          --nodes(table),        -- table of snippet nodes
+          --opts(table|nil)      -- *optional* table of additional snippet options
           --)
         end,
       },
@@ -45,23 +45,40 @@ return {
         documentation = cmp.config.window.bordered(),
       },
       mapping = cmp.mapping.preset.insert({
-      ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-      ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
-    sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      -- { name = 'vsnip' }, -- For vsnip users.
-      { name = 'luasnip' }, -- For luasnip users.
-      -- { name = 'ultisnips' }, -- For ultisnips users.
-      -- { name = 'snippy' }, -- For snippy users.
-      { name = 'buffer' }, -- text within current buffer
-      { name = 'path' }, -- file system paths
+        ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+        ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        -- { name = 'vsnip' }, -- For vsnip users.
+        { name = 'luasnip' }, -- For luasnip users.
+        -- { name = 'ultisnips' }, -- For ultisnips users.
+        -- { name = 'snippy' }, -- For snippy users.
+        { name = 'buffer' }, -- text within current buffer
+        { name = 'path' },   -- file system paths
+      }),
+      formatting = {
+        fields = { "kind", "abbr", "menu" },
+        -- 不要讓autocompletion 太長
+        format = function(entry, vim_item)
+          function trim(text)
+            local max = 40
+            if text and text:len() > max then
+              text = text:sub(1, max) .. "..."
+            end
+            return text
+          end
+
+          vim_item.abbr = trim(vim_item.abbr)
+
+          return vim_item
+        end,
+      }
     })
-  })
   end,
 }
